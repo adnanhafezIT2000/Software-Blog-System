496a0e09571457301ce3315a7b8bd1a3500e6cc2
adnan hafez <you@example.com>
2025-10-31 10:18:47 +0300

    (backend): install spatie laravel package, implement roles and permissions, and assign permssions to role

---
+++ software-blog-system-api/database/seeders/PermissionSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        # Permissions
+        $permissions = [
+            'browse posts' ,
+            'create new post' ,
+            'edit post' ,
+            'delete post' ,
+            'like on post' ,
+            'comment on post' ,
+            'report on post' ,
+            'manage report' ,
+            'manage category',
+            'manage tags' ,
+            'manage profile' ,
+        ];
+
+        foreach ($permissions as $permission) {
+
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        # Roles
+        $guest = Role::findByName('guest');
+        $user  = Role::findByName('user');
+        $admin = Role::findByName('admin');
+
+        # Assign Permission To Role
+        $guest->givePermissionTo('browse posts');
+
+        $user->givePermissionTo('browse posts');
+        $user->givePermissionTo('create new post');
+        $user->givePermissionTo('edit post');
+        $user->givePermissionTo('delete post');
+        $user->givePermissionTo('like on post');
+        $user->givePermissionTo('comment on post');
+        $user->givePermissionTo('report on post');
+        $user->givePermissionTo('manage profile');
+
+        $admin->givePermissionTo('delete post');
+        $admin->givePermissionTo('manage report');
+        $admin->givePermissionTo('manage category');
+        $admin->givePermissionTo('manage tags');
+    }
+}
